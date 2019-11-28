29f95a868dfc69e54f61b6ea50909e1bd22b44a6
Tanmay-Goel <53892924+Tanmay-Goel@users.noreply.github.com>
2019-11-28 22:53:53 +0530

    Create itemtagcheck.py

---
+++ itemtagcheck.py
@@ -0,0 +1,20 @@
+user_tags=['a','b','c']
+item_tags=['p','q','r']
+mydict={a:{p:1},b:{q:2, r:4}}
+tag='r'
+user='a'
+if tag in item_tags:
+    if user==''
+        print("Activate Trolley")
+    else:
+        for i,j in mydict.items():
+            if tag in j.keys():
+                for x,y in j:
+                    if x==tag:
+                        y+=1
+            else:
+                j[tag]=1
+                            
+        
+            
+            
